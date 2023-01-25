12310ee100b1af1b56b565c05913fea9f2e3684b
Divakar Kesarkar <divakark7020@gmail.com>
2023-01-25 18:29:35 +0530

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './component/about/about.component';
+import { AgentComponent } from './component/agent/agent.component';
+import { BlogComponent } from './component/blog/blog.component';
+import { ContactComponent } from './component/contact/contact.component';
+import { HomeComponent } from './component/home/home.component';
 
-const routes: Routes = [];
+import { PropertyComponent } from './component/property/property.component';
+
+const routes: Routes = [
+  {path:"home",component:HomeComponent},
+  {path:"about",component:AboutComponent},
+  {path:"property",component:PropertyComponent},
+  {path:"blog",component:BlogComponent},
+  {path:"agents",component:AgentComponent},
+  {path:"contact",component:ContactComponent},
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './component/header/header.component';
+import { FooterComponent } from './component/footer/footer.component';
+import { HomeComponent } from './component/home/home.component';
+import { AboutComponent } from './component/about/about.component';
+import { BlogComponent } from './component/blog/blog.component';
+
+import { ContactComponent } from './component/contact/contact.component';
+import { PropertyComponent } from './component/property/property.component';
+import { AgentComponent } from './component/agent/agent.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    AboutComponent,
+    BlogComponent,
+   
+    ContactComponent,
+    PropertyComponent,
+    AgentComponent
   ],
   imports: [
     BrowserModule,
